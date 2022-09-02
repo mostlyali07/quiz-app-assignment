0adf50e72c6d30fc08b2b7b5c2cfce7ba4a19af6
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-09-02 20:05:42 +0500

    update

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ function logIn(e) {
     var pass = document.getElementById('pass').value;
     var result = document.getElementById('result').value;
 
-    var user = localStorage.getItem(username);
+    var user = localStorage.setItem(username);
     var data = JSON.parse(user);
 }
